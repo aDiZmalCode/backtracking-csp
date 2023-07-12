1e97fd2a92621dbd4e868188e8f53ac9f144ec04
aDiZmalCode <deferdinand@gmail.com>
2023-07-12 11:24:24 -0400

    changed loop form while to for

--- src/index.py
+++ src/index.py
@@ -1,12 +1,19 @@
 # open graph file to be read
 f = open('./src/graph.txt','r')
+
 # first line is number of vertices, conver to integer
-vertices = int(f.readline(1))
- 
-i = 0
-arr = []
-while i <= vertices:
-    line = f.readline()
-    arr = line.split(" ")
-    i += 1
+numVertices = int(f.readline(1))
+
+# color domain
+colors = ["Red", "Green", "Blue", "Yellow", "Violet", "Gray", "Orange"]
+
+# vertices must be 2 and more, and 20 or less
+if numVertices >= 2 and numVertices <= 20:
+    for row in f:
+        vertexAdjacent = row.split(' ')
+        
+        for item in vertexAdjacent:
+            vertex = vertexAdjacent[0]
+            adjacent = vertexAdjacent[1:]
+        
 
